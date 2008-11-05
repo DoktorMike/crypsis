1de5c89741eb77689492a78d0fc134d0da332dd0
micke.green <micke.green@344834b4-9fbc-11dd-932a-8f5888711c47>
2008-11-05 20:45:01 +0000

    Made generators for backgrounds and for individuals.

--- Individual.cc
+++ Individual.cc
@@ -1,1 +1,9 @@
 #include "Individual.hh"
+
+Individual::Individual():fitness(0), genome(8)
+{
+}
+
+Individual::~Individual()
+{
+}
--- Individual.hh
+++ Individual.hh
@@ -7,14 +7,19 @@ class Individual
 {
 	public:
 
+		Individual();
+		~Individual();
 		double getFitness(){ return fitness; }
 		void setFitness(double fitness){ this->fitness = fitness; }
 
 		std::vector<double>& getGenome() { return genome; }
 
+		static const uint genomeSize = 8; /** The number of genes in the genome */
+		static const uint maxGenomeVal = 4; /** The maximum value a gene can have */
 	private:
-		double fitness;
-		std::vector<double> genome;
+		double fitness; /** The fitness of the individual */
+		std::vector<double> genome; /** The genome of the individual */
+
 };
 
 #endif
